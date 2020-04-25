94bf733e29dcee9a68ccd6c0396b75a35a6df336
Bhante Subhuti <bksubhuti@gmail.com>
2020-04-25 17:17:21 +0630

    set default to 350

--- tipitaka_projector_data/js/index_initialize.js
+++ tipitaka_projector_data/js/index_initialize.js
@@ -114,7 +114,7 @@
         document.write = localStorage.setItem("main_left", '0px'); 
     } 
     if (!localStorage.getItem("main_width")) {
-        document.write = localStorage.setItem("main_width", '300'); 
+        document.write = localStorage.setItem("main_width", '350'); 
     } 
     if (!localStorage.getItem("main_height")) {
         document.write = localStorage.setItem("main_height", '200px'); 
