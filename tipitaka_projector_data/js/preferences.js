10ba4db13bf9c1ab82217d0df47f84483e1a0426
vasil <sumbodhi@gmail.com>
2020-06-06 15:46:36 +0300

    reset all settings page refresh

--- tipitaka_projector_data/js/preferences.js
+++ tipitaka_projector_data/js/preferences.js
@@ -891,14 +891,8 @@ function hideshowlogo(){
 function ResetPreferences(){
         var askcf = confirm("It helps to refresh the Pāḷi Tipiṭaka Projector to its original settings.\nConfirm to proceed?");
         if (askcf == true) {
-  
-          for (i in localStorage) {
-            document.write.innerHTML = localStorage.setItem(i, '');
-            document.write.innerHTML = localStorage.removeItem(i);
-          } 
-  
-          window.location.pathname ='index.htm';
-  
+          localStorage.clear();
+          window.location.href ='index.htm';
         }
 }
 
