044f2bd16da1471a557e64258a3c94fb6e86d2bf
Bhante Subhuti <bksubhuti@gmail.com>
2020-09-17 20:14:15 +0630

    fix for dictionary lookup after search loadbook (does not look up words)

--- tipitaka_projector_data/js/jquery_stlookup.js
+++ tipitaka_projector_data/js/jquery_stlookup.js
@@ -280,6 +280,7 @@ function onWordSelected(t) {
 	localStorage.setItem('DictionaryKey', tdict);
 	$("#DeclensionKey").val(tdict);
 	document.write = localStorage.setItem('DeclensionKey', tdict);
+	DictionaryGo();
 
 	return t;
 }
